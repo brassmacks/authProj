9829c1b2b99dc284a36d40452b43937b290ce0c2
brassmacks <jadepavedpath@gmail.com>
2021-07-29 09:31:25 -0700

    bench bnb d1 wrap up

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,27 +1,24 @@
 class Api::SessionsController < ApplicationController
   def create
-    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-    unless @user
-      status 401
-    else 
+    @user = User.find_by_credentials(
+       params[:user][:username],
+       params[:user][:password])
+    if @user
       login!(@user)
       render 'api/users/show'
+    else 
+      render json: ["Invalid username or password"], status 401
     end
   end
 
   def destroy
-    logout!
-    render json: { message: 'Logout successful.' }
+    @user = current_user
+    if @user
+      logout!
+      render "api/users/show"
+    else
+    render json: ['No current user'], status: 404
+    end
   end
-
-
-
-  # def destroy
-  #   if @current_user
-  #     logout!
-  #   else
-  #     status 404
-  #   end
-  # end
 
 end
